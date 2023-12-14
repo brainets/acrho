059c26f726dc1befaade4625f130b0a95a5d5ebc
Matteo NERI <neri.m@BRAIN-LID-002.intlocal.univ-amu.fr>
2023-12-14 21:58:02 +0100

    adding one function for bootstrap, than more complete notebook, oinfo with stats and example on bootstrap

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,7 @@
 
 from itertools import combinations
 import numpy as np
+import hoi
 
 def oinfo_min(oinfo_array, list_indices,minsize):
 
@@ -30,3 +31,37 @@ def oinfo_min(oinfo_array, list_indices,minsize):
                     oi_array[n,t]=0
 
     return oi_array
+
+def goinfo_create_fit(data_brain, beh, minsize=3, maxsize=4):
+
+    model = hoi.metrics.GradientOinfo(data_brain, beh)
+    goinfo = model.fit(minsize=minsize, maxsize=maxsize)
+    return goinfo
+
+def goinfo_create_fit_correction(data_brain, beh, minsize=3, maxsize=4):
+
+    model = hoi.metrics.GradientOinfo(data_brain, beh)
+    goinfo = model.fit(minsize=minsize, maxsize=maxsize)
+
+    combos=model.get_combinations(minsize=minsize, maxsize=maxsize)[0]
+    list_indices=[[int(c) for c in comb if c != -1] for comb in combos]
+    list_multiplets=[str([int(i) for i in comb]) for comb in list_indices]
+
+    #Here we use the function defined in utils to "clean" the higher-order spreading
+    goinfo_proc=oinfo_min(goinfo, list_indices, minsize=minsize)
+
+    return goinfo_proc
+
+def bootstrap(data, nboot=1000):
+
+    """the data shuold be given in the form n_samples, n_features"""
+
+    n_sample, n_features = data.shape
+
+    data_boot=np.zeros((n_sample,n_features,nboot))
+    for i in range(nboot):
+        index=np.random.choice(np.arange(n_sample), n_sample)
+        data_boot[:,:,i]=data[index,:]
+
+    return data_boot
+
